408f71b1407b276b1f61869903f85141040ae6c9
Magda Zacharska <mzacharska@ebsco.com>
2019-02-12 11:47:27 -0500

    handling requests sent to  /item-storage/items endpoint

--- src/test/java/org/folio/circulation/pact/ItemPactTest.java
+++ src/test/java/org/folio/circulation/pact/ItemPactTest.java
@@ -2,14 +2,12 @@ package org.folio.circulation.pact;
 
 import static io.pactfoundation.consumer.dsl.LambdaDsl.newJsonBody;
 
-
 import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactProviderRuleMk2;
 import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.dsl.DslPart;
 import au.com.dius.pact.model.RequestResponsePact;
-
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,8 +21,8 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 
-
 import org.folio.circulation.domain.Item;
+import org.folio.circulation.resources.LoanCollectionResource;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.HttpResult;
 import org.folio.circulation.support.ItemRepository;
@@ -42,39 +40,36 @@ import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
-
 @RunWith(VertxUnitRunner.class)
 public class ItemPactTest {
-	
+
 	private Map<String, String> okapiHeaders = new HashMap<>();
-	//private Field mock = null;
-	 private Vertx vertx;
-	 private Router router;
+	// private Field mock = null;
+	private Vertx vertx;
+	private Router router;
 
-    @Rule
-    public PactProviderRuleMk2 provider = new PactProviderRuleMk2("item-storage", this);
-   
-    
-    @Before
-    public void setUp(TestContext context) throws Exception {
-      vertx = Vertx.vertx();  
-      router = Router.router(vertx);
-    }
+	@Rule
+	public PactProviderRuleMk2 provider = new PactProviderRuleMk2("item-storage", this);
 
-    @After
-    public void after(TestContext context) {
-      vertx.close(context.asyncAssertSuccess());
-    }
+	@Before
+	public void setUp(TestContext context) throws Exception {
+		vertx = Vertx.vertx();
+		router = Router.router(vertx);
+	}
 
+	@After
+	public void after(TestContext context) {
+		vertx.close(context.asyncAssertSuccess());
+	}
 
-    @Pact(provider = "item-storage", consumer = "mod-circulation")
-    public RequestResponsePact createPact(PactDslWithProvider builder) throws IOException {
-        
-    	final Map<String, String> headers = new HashMap<>();
-        headers.put("x-okapi-tenant", "tenant");
-        headers.put("x-okapi-token", "token");
-        
-        // expected  Pact
+	@Pact(provider = "item-storage", consumer = "mod-circulation")
+	public RequestResponsePact createPact(PactDslWithProvider builder) throws IOException {
+
+		final Map<String, String> headers = new HashMap<>();
+		/// headers.put("x-okapi-tenant", "tenant");
+		headers.put("x-okapi-token", "token");
+
+		// expected Pact
 		final DslPart itemBody = newJsonBody(o -> {
 			o.numberValue("totalResults", 1).array("items", item -> {
 				item.object(prop -> {
@@ -86,40 +81,42 @@ public class ItemPactTest {
 				});
 			});
 		}).build();
- 	
-    	return builder
-            .given("Items list")
-            .uponReceiving("request for an item")
-            .path("/item-storage/items/1234" )
-            .method("GET")
-            .headers(headers)
-            .willRespondWith()
-            .status(200)
-            .body(itemBody)
-            .toPact();
-    }
 
-     @Test
-     @PactVerification("item-storage")
-    public void pactTest(TestContext context) {
- 		 
-    	 final Async async = context.async();
-    	 router.route("/circulation/loans").handler(routingContext  ->  {
-   
-      	   WebContext webContext  = new WebContext(routingContext);
-      	   Clients clients= Clients.create(webContext, vertx.createHttpClient());
-      	   ItemRepository itemRepository = new ItemRepository(clients, true, true);      
-           CompletableFuture<HttpResult<Item>> result = itemRepository.fetchById("1234");
-           result.whenComplete((item,throwable)-> {
-          	  context.assertNotNull(item.value().getItemId());
-          	  context.assertNotNull(item.value().getStatus());
-          	  context.assertNotNull(item.value().getBarcode());
-          	  async.complete();
-              }).exceptionally(throwable -> {
-                  context.fail(throwable);
-                  async.complete();
-                  return null;
-                });  	 
-    	 });        
-     }
+		return builder.given("Items list").uponReceiving("request for an item").path("/item-storage/items/1234")
+				.method("GET").headers(headers).willRespondWith().status(200).body(itemBody).toPact();
+	}
+
+	@Test
+	@PactVerification("item-storage")
+	public void pactTest(TestContext context) {
+
+		HttpServer server = vertx.createHttpServer();
+		Async async = context.async();
+
+		HttpClient client = vertx.createHttpClient();
+		router.route("/circulation/loans").handler(routingContext -> {
+
+			WebContext webContext = new WebContext(routingContext);
+			Clients clients = Clients.create(webContext, client);
+			ItemRepository itemRepository = new ItemRepository(clients, false, false);
+			CompletableFuture<HttpResult<Item>> result = itemRepository.fetchById("1234");
+			result.whenComplete((item, throwable) -> {
+				context.assertNotNull(item.value().getItemId());
+				context.assertNotNull(item.value().getStatus());
+				context.assertNotNull(item.value().getBarcode());
+				async.complete();
+			});
+			routingContext.response().setStatusCode(200).end("OK");
+		}).failureHandler(handler -> {
+			System.out.println(handler.getBodyAsString() + "failing");
+
+		});
+		server.requestHandler(router::accept).listen(8080, "localhost", response -> {
+
+			client.getAbs("http://localhost:8080/circulation/loans").putHeader("x-okapi-url", provider.getUrl())
+					.putHeader("x-okapi-token", "token").handler(handler -> {
+					}).end();
+		});
+
+	}
 }
