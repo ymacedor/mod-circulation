6f76d08fab0df17912563f4180ddf1927d629d3f
Marc Johnson <marc@k-int.co.uk>
2020-02-20 14:09:46 +0000

    Check response in tests for fetching records using CQL CIRC-624

--- src/test/java/org/folio/circulation/support/fetching/FindMultipleRecordsUsingCqlTests.java
+++ src/test/java/org/folio/circulation/support/fetching/FindMultipleRecordsUsingCqlTests.java
@@ -1,15 +1,23 @@
 package org.folio.circulation.support.fetching;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toList;
 import static org.folio.circulation.support.http.client.CqlQuery.exactMatch;
 import static org.folio.circulation.support.http.client.PageLimit.limit;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
+import org.folio.circulation.domain.MultipleRecords;
 import org.folio.circulation.support.FindWithCqlQuery;
 import org.folio.circulation.support.GetManyRecordsClient;
 import org.folio.circulation.support.Result;
@@ -42,21 +50,47 @@ public class FindMultipleRecordsUsingCqlTests {
     final Result<CqlQuery> query = exactMatch("Status", "Open");
 
     when(client.getMany(eq(query.value()), eq(limit(10))))
-      .thenReturn(cannedResponse());
+      .thenReturn(cannedResponse(10));
 
-    fetcher.findByQuery(query);
+    MultipleRecords<JsonObject> records = getRecords(fetcher.findByQuery(query));
+
+    assertThat(records.getTotalRecords(), is(10));
+    assertThat(records.getRecords().size(), is(10));
 
     verify(client, times(1)).getMany(eq(query.value()), eq(limit(10)));
   }
 
-  private CompletableFuture<Result<Response>> cannedResponse() {
-    return CompletableFuture.completedFuture(Result.of(
-      () -> {
-        final JsonObject body = new JsonObject();
+  private CompletableFuture<Result<Response>> cannedResponse(
+      int numberOfRecordsInPage) {
 
-        body.put("records", new JsonArray());
+    return CompletableFuture.completedFuture(Result.of(() -> {
+      final JsonObject body = new JsonObject();
 
-        return new Response(200, body.encodePrettily(), "application/json");
-      }));
+      body.put("records", new JsonArray(generateRecords(numberOfRecordsInPage)));
+      body.put("totalRecords", numberOfRecordsInPage);
+
+      return new Response(200, body.encodePrettily(), "application/json");
+    }));
+  }
+
+  private List<JsonObject> generateRecords(int numberOfRecords) {
+    return Stream.generate(UUID::randomUUID)
+      .limit(numberOfRecords)
+      .map(id -> new JsonObject().put("id", id.toString()))
+      .collect(toList());
+  }
+
+  private MultipleRecords<JsonObject> getRecords(
+    CompletableFuture<Result<MultipleRecords<JsonObject>>> fetchedRecords) {
+
+    try {
+      final Result<MultipleRecords<JsonObject>> result = fetchedRecords
+        .get(1, SECONDS);
+
+      return result.value();
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
   }
 }
